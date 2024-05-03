c7412f26636062c26cc1f47749931b5de00e843c
jondujaka <jdujaka@gmail.com>
2024-05-03 16:10:06 +0200

    dujakle revamped

--- src/assets/parse.js
+++ src/assets/parse.js
@@ -4,7 +4,7 @@ const data = fs.readFileSync('all-words.txt', 'utf8')
 
 const words = data.split('\n')
 
-const parsedData = words.filter((word, i) => word.length && word.includes('d'))
+const parsedData = words.filter((word, i) => word.length && word.includes('g'))
 
 const uniqueWords = [...new Set(parsedData)]
 
