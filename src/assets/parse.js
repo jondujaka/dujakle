80de15d74ce4df78037d026298b078ee8b89aabe
jondujaka <jdujaka@gmail.com>
2024-05-04 12:27:27 +0200

    new word

--- src/assets/parse.js
+++ src/assets/parse.js
@@ -2,12 +2,12 @@ import fs from 'fs'
 
 const data = fs.readFileSync('all-words.txt', 'utf8')
 
-const words = data.split('\n')
+const words = data.split('\r\n')
 
-const parsedData = words.filter((word, i) => word.length && word.includes('g'))
+console.log(words)
+const parsedData = words.filter((word, i) => word.length && word.includes('d'))
 
 const uniqueWords = [...new Set(parsedData)]
 
-console.log(uniqueWords.length)
-
 fs.writeFileSync('words-parsed.json', JSON.stringify(uniqueWords))
+console.log('Successfully parsed word. Total results: ' + uniqueWords.length)
